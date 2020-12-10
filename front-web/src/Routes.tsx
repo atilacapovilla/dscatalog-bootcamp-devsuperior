953e403ab9a88284d34425f165be9543837110f0
atilacapovilla <capovillaatila@gmail.com>
2020-12-10 15:20:00 -0300

    Add base admin form structure

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
@@ -7,24 +7,25 @@ import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
 
 const Routes = () => (
-    <BrowserRouter>
-        <Navbar />
-        <Switch>
-            <Route path="/" exact>
-                <Home />
-            </Route>
-            <Route path="/products" exact>
-                <Catalog />
-            </Route>
-            <Route path="/products/:productId">
-                <ProductDetails />
-            </Route>
-            <Route path="/admin">
-                <Admin />
-            </Route>
-        </Switch>
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/products" exact>
+        <Catalog />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
+      </Route>
+      <Redirect from="/admin" to="/admin/products" exact />
+      <Route path="/admin">
+        <Admin />
+      </Route>
+    </Switch>
 
-    </BrowserRouter>
+  </BrowserRouter>
 );
 
 export default Routes;
